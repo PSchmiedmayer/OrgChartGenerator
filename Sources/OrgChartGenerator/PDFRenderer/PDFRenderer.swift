6922d5d27dc27e5b1c9c1ef757fbae9b026c4ece
Paul Schmiedmayer <paul.schmiedmayer@tum.de>
2019-06-04 17:48:21 +0200

    Improve PDF rendering without getting width and height using JavaScript

--- Sources/OrgChartGenerator/PDFRenderer/PDFRenderer.swift
+++ Sources/OrgChartGenerator/PDFRenderer/PDFRenderer.swift
@@ -13,12 +13,12 @@ final class PDFRenderer: NSObject, WebFrameLoadDelegate {
     private static let webFrameHandler = WebFrameHandler()
     
     private final class WebFrameHandler: NSObject, WebFrameLoadDelegate {
-        var callback: ((WebView) -> ())? = nil
+        var callback: ((WebView, WebFrame) -> ())? = nil
         
         func webView(_ sender: WebView, didFinishLoadFor frame: WebFrame!) {
             if sender.stringByEvaluatingJavaScript(from: "document.readyState") == "complete" {
                 sender.frameLoadDelegate = nil
-                callback?(sender)
+                callback?(sender, frame)
                 callback = nil
             }
         }
@@ -26,19 +26,20 @@ final class PDFRenderer: NSObject, WebFrameLoadDelegate {
     
     static func render(html: String, baseURL url: URL, completion: ((OrgChartError?) -> ())?) {
         webView.frameLoadDelegate = webFrameHandler
-        webFrameHandler.callback = { webView in
-            guard let height = Double(webView.stringByEvaluatingJavaScript(from: "document.body.scrollHeight")),
-                  let width = Double(webView.stringByEvaluatingJavaScript(from: "document.body.scrollWidth")) else {
-                return
-            }
-            
+        webFrameHandler.callback = { webView, frame in
             let printOpts: [NSPrintInfo.AttributeKey : Any] = [
                 NSPrintInfo.AttributeKey.jobDisposition: NSPrintInfo.JobDisposition.save,
                 NSPrintInfo.AttributeKey.jobSavingURL: url.appendingPathComponent("\(Generator.Constants.orgChartName).pdf", isDirectory: false)
             ]
             
             let printInfo: NSPrintInfo = NSPrintInfo(dictionary: printOpts)
-            printInfo.paperSize = CGSize(width: width, height: height)
+            printInfo.paperSize = frame.frameView.documentView.frame.size.applying(.init(scaleX: 0.8, y: 0.8))
+            printInfo.bottomMargin = 0.0
+            printInfo.topMargin = 0.0
+            printInfo.leftMargin = 0.0
+            printInfo.rightMargin = 0.0
+            printInfo.isVerticallyCentered = true
+            printInfo.isHorizontallyCentered = true
             
             let printOp: NSPrintOperation = NSPrintOperation(view: webView.mainFrame.frameView.documentView, printInfo: printInfo)
             
