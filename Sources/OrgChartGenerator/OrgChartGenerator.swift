468758917cbc1353d5dc9eda5601a88cdfc6775a
Paul Schmiedmayer <paul@schmiedmayer.de>
2020-05-19 10:19:18 +0200

    Fix that PDF was not created

--- Sources/OrgChartGenerator/OrgChartGenerator.swift
+++ Sources/OrgChartGenerator/OrgChartGenerator.swift
@@ -6,6 +6,7 @@
 //
 
 import ArgumentParser
+import Foundation
 
 
 struct OrgChartGenerator: ParsableCommand {
@@ -20,10 +21,51 @@ struct OrgChartGenerator: ParsableCommand {
     
 
     func run() throws {
-        Generator.generateOrgChart(in: path,
-                                   imageSize: imageSize,
-                                   compressionRate: compressionRate) { error in
+        let url = URL(fileURLWithPath: path, isDirectory: true)
+        
+        let progress = Progress(totalUnitCount: 100)
+        let observation = progress.observe(\.fractionCompleted, changeHandler: { process, _ in
+            print("🗂 is \(String(format: "%.1f", process.fractionCompleted * 100))% complete")
+        })
+        defer {
+            observation.invalidate()
+        }
+        
+        // Parse the file structure
+        progress.becomeCurrent(withPendingUnitCount: 5)
+        let orgChart = try OrgChart(fromDirectory: url)
+        progress.resignCurrent()
+        
+        // Create a temporary directory for the cropped pictures
+        let tempURL = url.appendingPathComponent(".pictures", isDirectory: true)
+        try? FileManager.default.removeItem(at: tempURL)
+        try FileManager.default.createDirectory(at: tempURL,
+                                                withIntermediateDirectories: true,
+                                                attributes: nil)
+        
+        // Crop the Faces
+        progress.becomeCurrent(withPendingUnitCount: 85)
+        try FaceCrop.crop(orgChart, tempURL: tempURL, imageSize: imageSize, compression: compressionRate)
+        progress.resignCurrent()
+        
+        // Render the HTML of the OrgChart
+        progress.becomeCurrent(withPendingUnitCount: 5)
+        let htmlData = try OrgChartHTMLRenderer.renderHTMLOrgChart(orgChart, in: url)
+        progress.resignCurrent()
+        
+        // Create a PDF from the HTML
+        progress.becomeCurrent(withPendingUnitCount: 4)
+        guard let htmlString = String(data: htmlData, encoding: .utf8) else {
+            throw GeneratorError.unknownError("Could not decode HTML data to String")
+        }
+        progress.resignCurrent()
+        
+        progress.becomeCurrent(withPendingUnitCount: 1)
+        PDFRenderer.render(html: htmlString, baseURL: url) { error in
+            progress.resignCurrent()
             OrgChartGenerator.exit(withError: error)
         }
+        
+        RunLoop.main.run()
     }
 }
